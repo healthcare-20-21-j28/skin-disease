4e3636570bc30927ced311ca5bcaaac4e7f7603d
healthcare-20-21-j28 <75713956+healthcare-20-21-j28@users.noreply.github.com>
2021-03-05 17:23:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ UPLOAD_FOLDER = './UPLOAD_FOLDER/'
 
 @app.route('/')
 def hello():
-    return 'Welcome to the HealthCare App Back-End !'
+    return 'Welcome to the HealthCare App BackEnd'
 
 
 @app.route('/disease/skin/test', methods=['POST', 'GET'])
@@ -23,9 +23,31 @@ def get_image():
     filename = UPLOAD_FOLDER + str(np.random.randint(0, 5000)) + '.png'
     print('Image is incoming')
     photo = request.files['photo']
-    # photo.save(filename)
+    photo.save(filename)
     print('Image Saved..')
-    return "done"
+    if check_skin(filename):
+        preds_dict = predict_class(filename)
+
+        dict_dis = sorted(preds_dict.items(), key=lambda x: x[1], reverse=True)
+        dict_dis = dict(sorted(preds_dict.items(), key=lambda x: x[1], reverse=True)[:3])
+        print(dict_dis)
+
+        max_val = max(dict_dis, key=dict_dis.get)
+        if dict_dis[max_val] <= 30:
+            print('healthy')
+            return jsonify({'message': 'Healthy Skin Detected'})
+        else:
+            print('Done')
+            description = (disease_description(str(max_val)))
+            return jsonify({'message': str(max_val), 'percentage': str(dict_dis[max_val]),
+                            'description': description['description'],
+                            'symptoms': description['symptoms'],
+                            'causes': description['causes'],
+                            'treatement-1': description['treatement-1'],
+                            'treatement-2': description['treatement-2']})
+    else:
+        print({'message': 'Please upload image of Infected Area'})
+        return jsonify({'message': 'Please upload image of Infected Area'})
 
 
 if __name__ == '__main__':
